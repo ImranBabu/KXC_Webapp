ae7c68452895d65788147f3f5286a7ac0482d802
ImranBabu <bimran357@gmail.com>
2020-09-18 07:38:19 -0700

    Fixed Issues and Added Registration module

--- app/models.py
+++ app/models.py
@@ -9,14 +9,14 @@ class User(UserMixin,db.Model):
 	id = db.Column(db.Integer ,primary_key = True)
 	username = db.Column(db.String(80),index= True , unique = True)
 	email = db.Column(db.String(150),index = True , unique = True)
-	password_hash = db.Column(db.String(100))
+	password = db.Column(db.String(100))
 	posts = db.relationship('Post',backref = 'author',lazy = 'dynamic')
 
 	def set_password(self,password):
-		self.password_hash = generate_password_hash(password)
+		self.password = generate_password_hash(password)
 
 	def verify_password(self,password):
-		return check_password_hash(self.password_hash,password)
+		return check_password_hash(self.password,password)
 
 	def __repr__(self):
 		return '<User {} Email {}>'.format(self.username,self.email)
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,10 @@
 from app import app
 from flask import render_template,flash,redirect,url_for,request
 from flask_login import current_user, login_user,logout_user,login_required
-from app.forms import LoginForm
+from app.forms import LoginForm,RegistrationForm
 from werkzeug.urls import url_parse
 from app.models import User,Post
+from app import db
 
 
 @app.route('/')
@@ -30,8 +31,22 @@ def login():
 		if not next_page or url_parse(next_page).netloc != '' :
 			flash("Welcome {} , your email {}".format(form.username.data,form.remember_me.data))
 			next_page = url_for('index')
-		return redirect(url_for(next_page))
+		return redirect(next_page)
 	return render_template('login.html',title = "Sign In",form = form)
+
+@app.route('/register',methods = ["GET","POST"])
+def register():
+	if current_user.is_authenticated:
+		return redirect(url_for("index"))
+	form = RegistrationForm()
+	if form.validate_on_submit():
+		user = User(username = form.username.data,email = form.email.data)
+		user.set_password(form.password.data)
+		db.session.add(user)
+		db.session.commit()
+		flash("Congratulations , You are added now")
+		return redirect(url_for("login"))
+	return render_template("register.html",title = "Register Page",form = form)
 
 @app.route('/logout')
 def logout():
